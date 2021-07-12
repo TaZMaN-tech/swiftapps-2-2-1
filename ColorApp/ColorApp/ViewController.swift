6c74ff4a9c172481603e9a2229d7676f9ae03904
TadevosKurdoglian <tazman311@gmail.com>
2021-07-12 08:23:31 +0300

    complite

--- ColorApp/ColorApp/ViewController.swift
+++ ColorApp/ColorApp/ViewController.swift
@@ -21,14 +21,28 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         viewColor.layer.cornerRadius = 15
+        
+        sliderRed.value = 0.35
+        sliderGreen.value = 0.55
+        sliderBlue.value = 0.75
+        
+        updateSliderText()
+        updateColorView()
     }
     
 
     @IBAction func sliderAction() {
+        updateSliderText()
+        updateColorView()
+    }
+    
+    func updateSliderText() {
         redLabel.text = String(format: "%.2f", sliderRed.value)
         greenLabel.text = String(format: "%.2f", sliderGreen.value)
         blueLabel.text = String(format: "%.2f", sliderBlue.value)
-        
+    }
+    
+    func updateColorView() {
         let red = CGFloat(sliderRed.value)
         let green = CGFloat(sliderGreen.value)
         let blue = CGFloat(sliderBlue.value)
